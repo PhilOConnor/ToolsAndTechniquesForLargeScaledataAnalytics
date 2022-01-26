3c1c5009d3d2d7ee063378409af72cb167735448
PhilOConnor <p.oconnor46@nuigalway.ie>
2022-01-26 17:48:44 +0000

    threading working, sorting is not :(

--- Assignment1/Sort.java
+++ Assignment1/Sort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
-public class Sort<T> { // Class Sort is a generic class with type parameter T
+public class Sort<T> extends Thread{ // Class Sort is a generic class with type parameter T
     T[] array; // The array of objects of type T we want to sort
     Comparator<T> comp; // A Comparator instance suitable for comparing objects of type T
     public static void main(String[] args) {
@@ -15,14 +15,14 @@ public class Sort<T> { // Class Sort is a generic class with type parameter T
             }
         };
 	*/
-	// Part B
-	// Creating a comparator called compString with expected input String
-	 Comparator<String> compString
-	 // Setting up a & b as Strings. Then using the comparison from the given code and repacking it into a lambda function 
-        	= (String a, String b) -> {if (a.compareTo(b) > 0){return 1;}else{return 0;}};
-        	
+        // Part B
+        // Creating a comparator called compString with expected input String
+        Comparator<String> compString
+                // Setting up a & b as Strings. Then using the comparison from the given code and repacking it into a lambda function
+                = (String a, String b) -> {if (a.compareTo(b) > 0){return 1;}else{return 0;}};
+
         Sort<String> sortStrings = new Sort<String>();
-        
+
 // Initialising an array of Strings with 16 unordered elements.
 // Array length must be a power of 2.
         String[] arrayOfStrings = { "Blue", "Yellow", "Almond", "Onyx", "Peach", "Gold",
@@ -31,7 +31,7 @@ public class Sort<T> { // Class Sort is a generic class with type parameter T
 // Sorting the array by calling the sort-method
         sortStrings.sort(arrayOfStrings, compString);
         System.out.println("Sorted array: " + Arrays.toString(arrayOfStrings));
-	
+
 	/*
 	 PART A
         // Creating a comparator of type Double and naming in compDouble, following the convention from above
@@ -45,20 +45,21 @@ public class Sort<T> { // Class Sort is a generic class with type parameter T
             }
         };
         */
-        
-	// PART B
-        Comparator<Double> compDouble 
-        	= (Double a, Double b) -> {if (a.compareTo(b) > 0){return 1;}else{return 0;}};
 
-        	
-	// Creating a new sort object to handle the Doubles
+        // PART B
+        Comparator<Double> compDouble
+                = (Double a, Double b) -> {if (a.compareTo(b) > 0){return 1;}else{return 0;}};
+
+
+        // Creating a new sort object to handle the Doubles
         Sort<Double> sortDouble = new Sort<Double>();
-	// Creating a new array of doubles to test out the sorting
+        // Creating a new array of doubles to test out the sorting
         Double[] arrayOfDoubles = new Double[]{ 36d,24d,32d,64d,10d, 15d, 19d, 20d, 12d,13d, 51d, 8d, 53d, 7d, 61d, 25d};
         System.out.println("Original array: " + Arrays.toString(arrayOfDoubles));
 // Sorting the array by calling the sort-method
         sortDouble.sort(arrayOfDoubles, compDouble);
         System.out.println("Sorted array: " + Arrays.toString(arrayOfDoubles));
+
     }
     public void sort(T[] array, Comparator<T> comp) { // Array length must be a power of 2
         this.array = array;
@@ -68,11 +69,29 @@ public class Sort<T> { // Class Sort is a generic class with type parameter T
     private void sort(int low, int n) {
         if (n > 1) {
             int mid = n >> 1;
-            sort(low, mid);
-            sort(low + mid, mid);
-            combine(low, n, 1);
+            // implementing a runnable interface within sort
+            Runnable s1 =
+                    () -> {sort(low, mid);};
+            Runnable s2 =
+                    () -> {sort(low + mid, mid);};
+            Runnable c =
+                    () -> {combine(low, n, 1);};
+
+            // creating the threads needed to run each runnable interface
+            Thread t1 = new Thread(s1);
+            Thread t2 = new Thread(s2);
+            Thread t3 = new Thread(c);
+            // Starting the threads and printing out the thread ID to check that new threads are being used for each object.
+            t1.start();
+            System.out.println(Thread.currentThread().getName());
+            t2.start();
+            System.out.println(Thread.currentThread().getName());
+            t3.start();
+            System.out.println(Thread.currentThread().getName());
+
         }
     }
+
     private void combine(int low, int n, int st) {
         int m = st << 1;
         if (m < n) {
