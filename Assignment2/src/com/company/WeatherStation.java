c2f7a5a9addddf696c0a662e7930a6d94f28ce01
PhilOConnor <p.oconnor46@nuigalway.ie>
2022-02-06 17:35:10 +0000

    Part A.1 complete - now adding maxTemp stream

--- Assignment2/src/com/company/WeatherStation.java
+++ Assignment2/src/com/company/WeatherStation.java
@@ -1,36 +1,70 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
 import java.util.List;
 
 public class WeatherStation {
-    String city;
-    //static String [] strArray = {"time", "temperature"};
-    //static List<Object> measurements = Arrays.asList(strArray);
-    static String[] measurements = {"time", "temperature"};
+    static String city;
+    // Creating a list of measurement objects
+    List<Object> measurements = new ArrayList<Object>();
+
     //Measurement measurements = new Measurement();
-    static List<String> stations;
+    static List<String> stations = new ArrayList<String>();
 
     double maxTemperature(int startTime, int endTime){
+        // Create a new empty stream to avoid returning nulls for streams with no outputs
+        //Stream<String> streamEmpty = Stream.empty();
         return 2;
-
     };
 
     public static void main(String[] args) {
-	// write your code here
-        WeatherStation w = new WeatherStation();
-        w.city = "Dublin";
-        System.out.println(w.city);
-        System.out.println(Arrays.toString(w.measurements));
+        // write your code here
 
+
+        stations.add("Dublin Airport"); // Weather Station 1 - ws1
+        stations.add("MET office");     // Weather Station 2 - ws2
+        city = "Dublin";
+
+        // Adding weather stations & adding measurements
+        WeatherStation w1 = new WeatherStation();
+        w1.measurements.add(new Measurement(2, 16d));
+        w1.measurements.add(new Measurement(2, 16d));
+        w1.measurements.add(new Measurement(3, 12d));
+        w1.measurements.add(new Measurement(4, 20d));
+        w1.measurements.add(new Measurement(5, 22d));
+
+        WeatherStation w2 = new WeatherStation();
+        w2.measurements.add(new Measurement(2, 13d));
+        w2.measurements.add(new Measurement(2, 14d));
+        w2.measurements.add(new Measurement(3, 16d));
+        w2.measurements.add(new Measurement(4, 14d));
+        w2.measurements.add(new Measurement(5, 25d));
+
+
+
+        // Checking that the daya is being stored properly
+        System.out.println(city);
+        System.out.println(stations);
+        System.out.println("Station 1 readings: "+w1.measurements);
+        System.out.println("Station 2 readings: "+w2.measurements);
 
 
     }
 
     static class Measurement{
-        int time;
-        double temperature;
-
-
+        // Class constructor
+        public Measurement(int time, double temperature){
+            this.time = time;
+            this.temperature = temperature;
+        };
+        // Overriding toString so I can see the values instead of location in memory
+        @Override
+        public String toString(){
+            return (this.time +":"+this.temperature);
+        }
+        public int time;
+        public double temperature;
     }
 }
