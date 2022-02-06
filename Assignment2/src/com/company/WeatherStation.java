390b5034f0f74778582a54fad26c9fa9d64145c5
PhilOConnor <p.oconnor46@nuigalway.ie>
2022-02-06 20:51:46 +0000

    Need to figure out streams - stuck on accessing temp data

--- Assignment2/src/com/company/WeatherStation.java
+++ Assignment2/src/com/company/WeatherStation.java
@@ -1,5 +1,5 @@
 package com.company;
-
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -14,8 +14,11 @@ public class WeatherStation {
     static List<String> stations = new ArrayList<String>();
 
     double maxTemperature(int startTime, int endTime){
-        // Create a new empty stream to avoid returning nulls for streams with no outputs
-        //Stream<String> streamEmpty = Stream.empty();
+        // Calculate the max temp between two timestamps using Java 8 streams, returning the max temp.
+        // Will require a filter and a max function.
+        //Stream<String> init = measurements.stream();
+        //Stream<Double> minTime =init.filter(temperature -> time>startTime).count();
+        //return minTime;
         return 2;
     };
 
@@ -35,6 +38,7 @@ public class WeatherStation {
         w1.measurements.add(new Measurement(4, 20d));
         w1.measurements.add(new Measurement(5, 22d));
 
+
         WeatherStation w2 = new WeatherStation();
         w2.measurements.add(new Measurement(2, 13d));
         w2.measurements.add(new Measurement(2, 14d));
@@ -44,13 +48,22 @@ public class WeatherStation {
 
 
 
-        // Checking that the daya is being stored properly
+        // Checking that the data is being stored properly
         System.out.println(city);
         System.out.println(stations);
         System.out.println("Station 1 readings: "+w1.measurements);
+        System.out.println("Station 1 counts "+w1.maxTemperature(1,3));
         System.out.println("Station 2 readings: "+w2.measurements);
+        System.out.println("Station 2 readings: "+w2.measurements.get(1));
 
 
+        //w2.measurements.stream().allMatch(time -> valueInt(w2.measurements.get(0))>1);
+        /*Integer max = w1.measurements
+                .stream()
+                .mapToInt(v -> v[0])
+                .max().orElseThrow(NoSuchElementException::new);
+
+         */
     }
 
     static class Measurement{
@@ -63,8 +76,10 @@ public class WeatherStation {
         @Override
         public String toString(){
             return (this.time +":"+this.temperature);
-        }
+       }
         public int time;
         public double temperature;
     }
+
+
 }
