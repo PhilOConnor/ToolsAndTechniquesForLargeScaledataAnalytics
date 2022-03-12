52294d751e21d45784812db93afa44386ca60269
PhilOConnor <p.oconnor46@nuigalway.ie>
2022-03-12 13:45:43 +0000

    Completed assignment

--- Assignment4/src/com/company/kMeans.java
+++ Assignment4/src/com/company/kMeans.java
@@ -21,6 +21,10 @@ import java.util.List;
 
 public class kMeans {
     // Creating the javabean to store the tweets
+    // 3 rules for java beans
+    // 1 - implements Serializable
+    // 2 - Blank constructor
+    // 3 - All properties are private with public getters and setters
     public static class AnnotatedTweet implements java.io.Serializable{
         private String tweet;
         private int spam;
@@ -61,18 +65,19 @@ public class kMeans {
         JavaRDD<Tuple2<Vector, AnnotatedTweet>> parsedData = data
                 .map(y -> {
                     // Bharath said this isn't the best way to split the data as there may be commas in the tweets,
-                    // in this case there isnt but I'm aware it could be an issue
+                    // in this case there isn't but I'm aware it could be an issue. Split each tweet item into an array of strings
                     String[] values = y.split(",");
                     // Parse the tweet data into the needed features
-                    int spam = Integer.parseInt(values[0]);
-                    String tweet = values[1];
-                    Double xCoord = Double.parseDouble(values[2]);
-                    Double yCoord = Double.parseDouble(values[3]);
-                    AnnotatedTweet annotatedTweet = new AnnotatedTweet();
-                    annotatedTweet.setTweet(tweet);
+                    int spam = Integer.parseInt(values[0]); // The spam feature
+                    String tweet = values[1]; // The tweet main body
+                    Double xCoord = Double.parseDouble(values[2]); // X coordinate
+                    Double yCoord = Double.parseDouble(values[3]); // y coordinate
+                    AnnotatedTweet annotatedTweet = new AnnotatedTweet(); // Create a new annotatedTweet from the Java Bean
+                    annotatedTweet.setTweet(tweet); // Set the tweet and spam properties of the annotatedTweet object
                     annotatedTweet.setSpam(spam);
 
-                    // Return the tuple from the code block
+                    // Return the tuple from the code block as a tuple2 with the coordinates as a dense vector and the annotatedTweet
+                    // Tuple2 won't allow for 3 items so tweet and spam flag had to be encoded in the annotatedTweet object.
                     return new Tuple2<Vector, AnnotatedTweet>(Vectors.dense(xCoord,yCoord), annotatedTweet);
                 });
         // Bharath provided the below as a skeleton
@@ -86,11 +91,13 @@ public class kMeans {
         // Sort by taken from https://www.tabnine.com/code/java/methods/org.apache.spark.api.java.JavaRDD/sortBy
         tweetsWithClusterIndices.sortBy(Tuple2::_2,true, 2).foreach(y-> System.out.println("Tweet " + y._1.getTweet() + " is in cluster " + y._2));
 
-        //
+        // Create a filter for only the spam tweets
         JavaRDD<Tuple2<AnnotatedTweet, Integer>> spamOnly = tweetsWithClusterIndices.filter(y->y._1.getSpam()==1);
+        // Below was inspired by the wordCount example when setting up Spark
+        // For every spam tweet, create JavaPairRDD so that the rdd is (ClusterID, 1)
         JavaPairRDD<Integer/*Cluster*/, Integer/*Ones*/> ones =  spamOnly.mapToPair(y -> new Tuple2<Integer, Integer>(y._2, 1));
+        // For each clusterID, sum up all the 1s and collect them.
         JavaPairRDD<Integer, Integer> counts = ones.reduceByKey((Integer i1, Integer i2) -> i1 + i2);
-
         List<Tuple2<Integer, Integer>> output = counts.collect();
 
         for (Tuple2<?, ?> tuple : output)
